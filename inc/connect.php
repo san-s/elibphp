c5ad565ff316d44240752f4488160da91c84866f
san-s <42447997+san-s@users.noreply.github.com>
2023-03-28 21:46:57 +0100

    Create connect.php

---
+++ inc/connect.php
@@ -0,0 +1,5 @@
+<?php 
+$con = mysqli_init();
+mysqli_ssl_set($con,NULL,NULL, "{path to CA cert}", NULL, NULL);
+mysqli_real_connect($conn, "elibrary1.mysql.database.azure.com", "cloudsec", "Test@123", "{your_database}", 3306, MYSQLI_CLIENT_SSL);
+}
